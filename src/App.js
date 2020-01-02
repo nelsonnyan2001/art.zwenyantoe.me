cbbb4324668d77778792161f9f81214cd3f8b7cc
nelsonnyan2001 <zwenyantoe@my.jcu.edu.au>
2020-01-03 01:57:26 +0800

    Refined website

--- src/App.js
+++ src/App.js
@@ -5,16 +5,28 @@ import './App.css';
 function App() {
   return (
     <div className = "container-main">
+      <img 
+      src = {require('./lain-laughing.gif')}
+      alt = "Lain laughing"
+      className = "image-container"></img>
       <div className="main-text-wrapper">
-        <p> the tea that I drank </p>
-        <p> at the corner store we love </p>
-        <p> sweet and cold </p>
-        <p> just how i like it </p>
-        <p> i don't know how </p>
-        <p> you push all the right buttons </p>
-        <p> sweet but cold </p>
-        <p> well then again, you always were </p>
+        <p> the tea that I drank <br/>
+        <br/> at the corner store we love <br/>
+        <br/> was sweet and cold <br/>
+        <br/> just how i liked it <br/>
+        <br/> i don't know how <br/>
+        <br/> you push all the right buttons <br/>
+        <br/> but this tea isn't nearly as sweet <br/>
+        <br/> without you <br/>
+        <br/>
+        <br/> zwe
+        </p>
       </div>
+      <img 
+      src = {require('./lain-yearning.gif')}
+      alt = "Sad Lain"
+      className = "image-container"></img>
+      <div className="main-text-wrapper"></div>
     </div>
     
   );
