ad05c33df48737b045e893cca8698d34b38a6fd0
nelsonnyan2001 <zwenyantoe@my.jcu.edu.au>
2020-01-02 23:57:19 +0800

    First proper redesign

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p> took a bite of your apple </p>
-      </header>
+    <div className = "container-main">
+      <div className="main-text-wrapper">
+        <p> the tea that I drank </p>
+        <p> at the corner store we love </p>
+        <p> sweet and cold </p>
+        <p> just how i like it </p>
+        <p> i don't know how </p>
+        <p> you push all the right buttons </p>
+        <p> sweet but cold </p>
+        <p> well then again, you always were </p>
+      </div>
     </div>
+    
   );
 }
 
