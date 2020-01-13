91834efe37c496668b584567831e2ff44bdacbc4
nelsonnyan2001 <zwenyantoe@my.jcu.edu.au>
2020-01-13 13:07:55 +0800

    Cleaned up moon page

--- src/Screens/moon/Moon.js
+++ src/Screens/moon/Moon.js
@@ -6,23 +6,23 @@ class Home extends React.Component {
     render() {
         return (
             <div className="container-moon">
+                <div className="moon-title-wrapper">
+                    <p>moon</p>
+                </div>
                 <img src={require('../../images/moonflicker.gif')} />
                 <div className="moon-text-wrapper">
-                    <div className="moon-title-wrapper">
-                        <p>moon</p>
-                    </div>
                     <p> i look at you <br />
-                    <br /> in your yellow dress <br />
-                    <br /> and i look at the moon - <br />
-                    <br /> its silver light in excess <br />
-                    <br /> and just cause the moon <br />
-                    <br /> waxes and wanes <br />
-                    <br /> i don't think it's fair <br />
-                    <br /> that you do the same <br />
+                        <br /> in your yellow dress <br />
+                        <br /> and i look at the moon - <br />
+                        <br /> its silver light in excess <br />
+                        <br /> and just cause the moon <br />
+                        <br /> waxes and wanes <br />
+                        <br /> i don't think it's fair <br />
+                        <br /> that you do the same <br />
                     </p>
                     <br />
                     <Link to="/"
-                    className="link-moon">
+                        className="link-moon">
                         «--main--«
                     </Link>
                 </div>
